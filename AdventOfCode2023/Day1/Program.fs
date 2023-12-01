dd886cebe591b83b58226b2de70485bba865686c
Justin <jstnlefebvre@gmail.com>
2023-12-01 15:29:49 -0500

    Finished day 1!

--- AdventOfCode2023/Day1/Program.fs
+++ AdventOfCode2023/Day1/Program.fs
@@ -7,9 +7,20 @@ type CalibrationValue = int
 type CalibrationDocument = int seq
 
 module CalibrationDocument =
-  let parse filename : CalibrationDocument =
-    let findValue line =
-      let digits = line |> String.filter Char.IsDigit
+  let wordDigits =
+    Map["one", "1"
+        "two", "2"
+        "three", "3"
+        "four", "4"
+        "five", "5"
+        "six", "6"
+        "seven", "7"
+        "eight", "8"
+        "nine", "9"]
+
+  let parse predicate filename : CalibrationDocument =
+    let findValue (transform: string -> string) line =
+      let digits = line |> transform
       String [| digits[0]; digits[digits.Length - 1] |] |> Int32.Parse
 
-    File.ReadLines filename |> Seq.map findValue
+    File.ReadLines filename |> Seq.map (findValue predicate)
--- AdventOfCode2023/Day1/Tests.fs
+++ AdventOfCode2023/Day1/Tests.fs
@@ -1,10 +1,39 @@
 module Day1.Tests
 
+open System
 open Xunit
 
 [<Theory>]
 [<InlineData("Day1/testInput.txt", 142)>]
 [<InlineData("Day1/input.txt", 55712)>]
 let ``The sum of calibration values`` (filename: string, expected: int) =
-  let result = CalibrationDocument.parse filename |> Seq.sum
+  let result =
+    CalibrationDocument.parse (String.filter Char.IsDigit) filename |> Seq.sum
+
+  Assert.Equal(expected, result)
+
+
+[<Theory>]
+[<InlineData("Day1/testInput2.txt", 281)>]
+[<InlineData("Day1/input.txt", 55413)>]
+let ``The sum of calibration values with word digits`` (filename: string, expected: int) =
+  let transformToDigits (s: string) =
+    let findWordDigit (subString: string) =
+      CalibrationDocument.wordDigits.Keys
+      |> Seq.tryFind subString.StartsWith
+      |> Option.map (fun w -> CalibrationDocument.wordDigits[w])
+
+    let mutable ret = []
+
+    for i, c in s |> Seq.indexed do
+      if Char.IsDigit c then
+        ret <- ret @ [ Char.ToString c ]
+      else
+        match findWordDigit s[i..] with
+        | Some(d) -> ret <- ret @ [ d ]
+        | _ -> ()
+
+    String.Join("", ret)
+
+  let result = CalibrationDocument.parse transformToDigits filename |> Seq.sum
   Assert.Equal(expected, result)
