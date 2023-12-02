9394b9d1f3c79999f6a2fe9f8608ee6a3b889d9a
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-02 16:11:57 -0500

    stub day 4

--- AdventOfCode2023/Day4/Tests.fs
+++ AdventOfCode2023/Day4/Tests.fs
@@ -1,18 +1,18 @@
-module Day3.Tests
+module Day4.Tests
 
 open Xunit
 
 [<Theory>]
-[<InlineData("Day3/testInput.txt", 0)>]
-[<InlineData("Day3/input.txt", 0)>]
+[<InlineData("Day4/testInput.txt", 0)>]
+[<InlineData("Day4/input.txt", 0)>]
 let ``test`` (filename: string, expected: int) =
   let result = -1
   Assert.Equal(expected, result)
 
 
 [<Theory>]
-[<InlineData("Day3/testInput.txt", 0)>]
-[<InlineData("Day3/input.txt", 0)>]
+[<InlineData("Day4/testInput.txt", 0)>]
+[<InlineData("Day4/input.txt", 0)>]
 let ``test 2`` (filename: string, expected: int) =
   let result = -1
   Assert.Equal(expected, result)
