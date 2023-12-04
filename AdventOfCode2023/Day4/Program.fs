b5ec1bab134591923af3c025fd90ff573f3bca8a
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-04 00:52:42 -0500

    Finished Day4 part 1

--- AdventOfCode2023/Day4/Program.fs
+++ AdventOfCode2023/Day4/Program.fs
@@ -1,1 +1,37 @@
 namespace Day4
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type ScratchCard =
+  { id: string
+    winning: int Set
+    mine: int Set }
+
+module ScratchCard =
+  let score card =
+    let intersect = Set.intersect card.winning card.mine
+    pown 2 (intersect.Count - 1)
+
+type ScratchCards = ScratchCard seq
+
+module ScratchCards =
+  let private regex =
+    Regex("^Card +(?<id>\d+): +(?<winning>\d+ *)+\| +(?<mine>\d+ *)+$")
+
+  let parse filename : ScratchCards =
+    let parseLine line : ScratchCard =
+      let m = regex.Match line
+
+      let winning =
+        m.Groups["winning"].Captures |> Seq.map (fun c -> Int32.Parse(c.Value)) |> Set
+
+      let mine =
+        m.Groups["mine"].Captures |> Seq.map (fun c -> Int32.Parse(c.Value)) |> Set
+
+      { id = m.Groups["id"].Value
+        winning = winning
+        mine = mine }
+
+    filename |> File.ReadLines |> Seq.map parseLine
--- AdventOfCode2023/Day4/Tests.fs
+++ AdventOfCode2023/Day4/Tests.fs
@@ -3,10 +3,10 @@ module Day4.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day4/testInput.txt", -1)>]
-[<InlineData("Day4/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day4/testInput.txt", 13)>]
+[<InlineData("Day4/input.txt", 18653)>]
+let ``Total points of the winning scratchcards`` (filename: string, expected: int) =
+  let result = filename |> ScratchCards.parse |> Seq.map ScratchCard.score |> Seq.sum
   Assert.Equal(expected, result)
 
 
