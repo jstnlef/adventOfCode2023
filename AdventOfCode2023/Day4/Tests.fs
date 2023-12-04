8aedc8cf0521062dcf8ac806a00bb774f05e9ca1
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-03 19:24:35 -0500

    Add day 6

--- AdventOfCode2023/Day4/Tests.fs
+++ AdventOfCode2023/Day4/Tests.fs
@@ -3,16 +3,16 @@ module Day4.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day4/testInput.txt", 0)>]
-[<InlineData("Day4/input.txt", 0)>]
+[<InlineData("Day4/testInput.txt", -1)>]
+[<InlineData("Day4/input.txt", -1)>]
 let ``test`` (filename: string, expected: int) =
-  let result = -1
+  let result = 0
   Assert.Equal(expected, result)
 
 
 [<Theory>]
-[<InlineData("Day4/testInput.txt", 0)>]
-[<InlineData("Day4/input.txt", 0)>]
+[<InlineData("Day4/testInput.txt", -1)>]
+[<InlineData("Day4/input.txt", -1)>]
 let ``test 2`` (filename: string, expected: int) =
-  let result = -1
+  let result = 0
   Assert.Equal(expected, result)
--- AdventOfCode2023/Day6/Tests.fs
+++ AdventOfCode2023/Day6/Tests.fs
@@ -1,18 +1,18 @@
-module Day5.Tests
+module Day6.Tests
 
 open Xunit
 
 [<Theory>]
-[<InlineData("Day5/testInput.txt", -1)>]
-[<InlineData("Day5/input.txt", -1)>]
+[<InlineData("Day6/testInput.txt", -1)>]
+[<InlineData("Day6/input.txt", -1)>]
 let ``test`` (filename: string, expected: int) =
   let result = 0
   Assert.Equal(expected, result)
 
 
 [<Theory>]
-[<InlineData("Day5/testInput.txt", -1)>]
-[<InlineData("Day5/input.txt", -1)>]
+[<InlineData("Day6/testInput.txt", -1)>]
+[<InlineData("Day6/input.txt", -1)>]
 let ``test 2`` (filename: string, expected: int) =
   let result = 0
   Assert.Equal(expected, result)
