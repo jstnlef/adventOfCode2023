2f31f41ca90a94c408c51ff8e0a9bb20ced26225
Justin <jstnlefebvre@gmail.com>
2023-12-05 14:41:36 -0500

    Day 5 part 1 done

--- AdventOfCode2023/Day5/Program.fs
+++ AdventOfCode2023/Day5/Program.fs
@@ -3,26 +3,59 @@ namespace Day5
 open System
 open System.IO
 
+open System.Text.RegularExpressions
+open Common.BTree
+
 type Seed = int64
 
-type Range =
-  { source: int64
-    destination: int64
-    length: int64 }
+type Range(start: int64, length: int64) =
+  member x.Start = start
+  member x.Length = length
+  override x.GetHashCode() = hash (start, length)
 
-module Range =
-  let doTransform n range = n
+  override x.Equals(b) =
+    match b with
+    | :? Range as r -> (start, length) = (r.Start, r.Length)
+    | _ -> false
 
-type Almanac = { seeds: Seed Set }
+  interface IComparable with
+    member x.CompareTo yobj =
+      match yobj with
+      | :? Range as r -> start.CompareTo(r.Start)
+      | :? Seed as n ->
+        if n >= start && n <= (start + length) then 0
+        elif n < start then -1
+        else 1
+      | _ -> invalidArg "yobj" "cannot compare values of different types"
+
+type Transform = { range: Range; destination: int64 }
+
+type RangeMap =
+  { name: string
+    transforms: Tree<Range, Transform> }
+
+module RangeMap =
+  let doTransform n rangeMap =
+    rangeMap.transforms
+    |> searchTreeBy (fun r -> (r :> IComparable).CompareTo(n))
+    |> Option.map (fun range -> range.destination + (n - range.range.Start))
+    |> Option.defaultValue n
+
+type Almanac =
+  { seeds: Seed Set
+    maps: RangeMap array }
 
 module Almanac =
-  let findLocation seed almanac = seed
+  let findLocation almanac seed =
+    almanac.maps |> Seq.fold RangeMap.doTransform seed
 
   let findLowestLocation almanac =
-    almanac.seeds |> Seq.minBy (findLocation almanac)
+    almanac.seeds |> Seq.map (findLocation almanac) |> Seq.min
 
   let parse filename =
-    let lines = filename |> File.ReadAllLines
+    let mapRegex = Regex("(?<name>[\w|\-?]+) map:\n((?<range>\d+ ?)\n?)+")
+
+    let lines = filename |> File.ReadAllText |> (fun s -> s.Split("\n\n"))
 
     let parseLineToIn64Set (s: string) =
       s.Trim().Split [| ' ' |] |> Array.map Int64.Parse |> Set
@@ -31,4 +64,26 @@ module Almanac =
       (line.Split [| ':' |])[1] |> parseLineToIn64Set
 
     let seeds = parseSeeds lines[0]
-    { seeds = seeds }
+
+    let parseMap (mapText: string) =
+      let m = mapRegex.Match(mapText)
+
+      let rangeMatch =
+        m.Groups["range"].Captures
+        |> Seq.map (fun c -> Int64.Parse c.Value)
+        |> Seq.chunkBySize 3
+
+      let transforms =
+        rangeMatch
+        |> Seq.fold
+          (fun tree nums ->
+            let range = Range(nums[1], nums[2])
+            insertNode range { range = range; destination = nums[0] } tree)
+          Tree.Empty
+
+      { name = m.Groups["name"].Value
+        transforms = transforms }
+
+    let maps = lines[1..] |> Array.map parseMap
+
+    { seeds = seeds; maps = maps }
--- AdventOfCode2023/Day5/Tests.fs
+++ AdventOfCode2023/Day5/Tests.fs
@@ -4,7 +4,7 @@ open Xunit
 
 [<Theory>]
 [<InlineData("Day5/testInput.txt", 35)>]
-[<InlineData("Day5/input.txt", -1)>]
+[<InlineData("Day5/input.txt", 318728750)>]
 let ``The lowest location number that corresponds to any of the initial seeds`` (filename: string, expected: int64) =
   let result = filename |> Almanac.parse |> Almanac.findLowestLocation
   Assert.Equal(expected, result)
