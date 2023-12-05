60abe4308f72c08aeba700ee4dcf30c74257882f
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-05 01:00:35 -0500

    started day5

--- AdventOfCode2023/Day5/Program.fs
+++ AdventOfCode2023/Day5/Program.fs
@@ -1,1 +1,34 @@
 namespace Day5
+
+open System
+open System.IO
+
+type Seed = int64
+
+type Range =
+  { source: int64
+    destination: int64
+    length: int64 }
+
+module Range =
+  let doTransform n range = n
+
+type Almanac = { seeds: Seed Set }
+
+module Almanac =
+  let findLocation seed almanac = seed
+
+  let findLowestLocation almanac =
+    almanac.seeds |> Seq.minBy (findLocation almanac)
+
+  let parse filename =
+    let lines = filename |> File.ReadAllLines
+
+    let parseLineToIn64Set (s: string) =
+      s.Trim().Split [| ' ' |] |> Array.map Int64.Parse |> Set
+
+    let parseSeeds (line: string) =
+      (line.Split [| ':' |])[1] |> parseLineToIn64Set
+
+    let seeds = parseSeeds lines[0]
+    { seeds = seeds }
--- AdventOfCode2023/Day5/Tests.fs
+++ AdventOfCode2023/Day5/Tests.fs
@@ -3,16 +3,16 @@ module Day5.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day5/testInput.txt", -1)>]
+[<InlineData("Day5/testInput.txt", 35)>]
 [<InlineData("Day5/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``The lowest location number that corresponds to any of the initial seeds`` (filename: string, expected: int64) =
+  let result = filename |> Almanac.parse |> Almanac.findLowestLocation
   Assert.Equal(expected, result)
 
 
 [<Theory>]
 [<InlineData("Day5/testInput.txt", -1)>]
 [<InlineData("Day5/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
+let ``test 2`` (filename: string, expected: int64) =
   let result = 0
   Assert.Equal(expected, result)
