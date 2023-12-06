0bc9fad8a74bda703a0e9750351c75dc38e62817
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-06 01:13:19 -0500

    Day 6 part 1

--- AdventOfCode2023/Day6/Program.fs
+++ AdventOfCode2023/Day6/Program.fs
@@ -1,1 +1,48 @@
 namespace Day6
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Race = int * int
+
+type RaceDocument =
+  { times: int array
+    distances: int array }
+
+module RaceDocument =
+  let getRace id doc : Race = doc.times[id], doc.distances[id]
+
+  let distanceForHeldTime time heldTime = (time - heldTime) * heldTime
+
+  let numWaysToBeatRecord (time: int, distance: int) =
+    let lower =
+      seq { (time / 2) - 1 .. -1 .. 0 }
+      |> Seq.takeWhile (fun heldTime -> distanceForHeldTime time heldTime > distance)
+
+    let upper =
+      seq { time / 2 .. time }
+      |> Seq.takeWhile (fun heldTime -> distanceForHeldTime time heldTime > distance)
+
+    Seq.append lower upper |> Seq.toList |> (fun l -> l.Length)
+
+  let multipliedNumOfWaysToBeatRecord doc =
+    Array.zip doc.times doc.distances
+    |> Array.map numWaysToBeatRecord
+    |> Array.fold (*) 1
+
+  let parse filename : RaceDocument =
+    let regex = Regex("\w+:( +(?<num>\d+))+")
+    let lines = filename |> File.ReadAllLines
+
+    let times =
+      regex.Match(lines[0]).Groups["num"].Captures
+      |> Seq.map (fun c -> Int32.Parse(c.Value))
+      |> Seq.toArray
+
+    let distances =
+      regex.Match(lines[1]).Groups["num"].Captures
+      |> Seq.map (fun c -> Int32.Parse(c.Value))
+      |> Seq.toArray
+
+    { times = times; distances = distances }
--- AdventOfCode2023/Day6/Tests.fs
+++ AdventOfCode2023/Day6/Tests.fs
@@ -3,10 +3,12 @@ module Day6.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day6/testInput.txt", -1)>]
-[<InlineData("Day6/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day6/testInput.txt", 288)>]
+[<InlineData("Day6/input.txt", 840336)>]
+let ``Multiple of the number of ways you could beat the record in each race`` (filename: string, expected: int) =
+  let result =
+    filename |> RaceDocument.parse |> RaceDocument.multipliedNumOfWaysToBeatRecord
+
   Assert.Equal(expected, result)
 
 
