8e3c7b10ac1eb88f48644030676a33f9c9cdaa5a
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-07 01:15:29 -0500

    Day7 part 1 partial

--- AdventOfCode2023/Day7/Program.fs
+++ AdventOfCode2023/Day7/Program.fs
@@ -1,1 +1,45 @@
 namespace Day7
+
+open System
+open System.IO
+
+type Hand = string
+type Bid = int
+type CamelHands = (Hand * Bid) array
+
+type Type =
+  | HighCard = 0
+  | OnePair = 100
+  | TwoPair = 200
+  | ThreeOfAKind = 300
+  | FullHouse = 400
+  | FourOfAKind = 500
+  | FiveOfAKind = 600
+
+module Hand =
+  let kind hand = Type.FiveOfAKind
+
+  let value card =
+    match card with
+    | 'A' -> 14
+    | 'K' -> 13
+    | 'Q' -> 12
+    | 'J' -> 11
+    | 'T' -> 10
+    | c -> Int32.Parse(string c)
+
+  let strength (hand: Hand, _) = (int (kind hand)) + (value hand[0])
+
+  let score rank (_, bid) = bid * (rank + 1)
+
+module CamelHands =
+  let totalWinnings (hands: CamelHands) =
+    hands |> Array.sortBy Hand.strength |> Array.mapi Hand.score |> Array.sum
+
+  let parse filename =
+    filename
+    |> File.ReadLines
+    |> Seq.map (fun s ->
+      let split = s.Split(" ")
+      split[0], split[1] |> Int32.Parse)
+    |> Seq.toArray
--- AdventOfCode2023/Day7/Tests.fs
+++ AdventOfCode2023/Day7/Tests.fs
@@ -3,10 +3,10 @@ module Day7.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day7/testInput.txt", -1)>]
+[<InlineData("Day7/testInput.txt", 6440)>]
 [<InlineData("Day7/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``The total winnings of the Camel Cards hands`` (filename: string, expected: int) =
+  let result = filename |> CamelHands.parse |> CamelHands.totalWinnings
   Assert.Equal(expected, result)
 
 
