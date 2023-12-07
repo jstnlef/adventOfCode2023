deab340957a54af3c8c3d8cbf27e77cbe5eb9e77
Justin <jstnlefebvre@gmail.com>
2023-12-07 13:50:59 -0500

    Day 7 part 2 done!

--- AdventOfCode2023/Day7/Program.fs
+++ AdventOfCode2023/Day7/Program.fs
@@ -9,12 +9,12 @@ type CamelHands = (Hand * Bid) array
 
 type Type =
   | HighCard = 0
-  | OnePair = 1000
-  | TwoPair = 2000
-  | ThreeOfAKind = 3000
-  | FullHouse = 4000
-  | FourOfAKind = 5000
-  | FiveOfAKind = 6000
+  | OnePair = 1
+  | TwoPair = 2
+  | ThreeOfAKind = 3
+  | FullHouse = 4
+  | FourOfAKind = 5
+  | FiveOfAKind = 6
 
 module Hand =
   let countCards (hand: Hand) =
@@ -27,11 +27,9 @@ module Hand =
           | Some(n) -> Some(n + 1)
           | None -> Some(1)))
       Map.empty
-    |> Map.toArray
-    |> Array.sortByDescending snd
 
-  let kind (hand: Hand) =
-    let counts = countCards hand
+  let kindWithJacks (hand: Hand) =
+    let counts = countCards hand |> Map.toArray |> Array.sortByDescending snd
 
     match counts[0] with
     | _, count when count = 5 -> Type.FiveOfAKind
@@ -42,37 +40,77 @@ module Hand =
     | _, count when count = 2 -> Type.OnePair
     | _ -> Type.HighCard
 
-  let compareHighCards (hand: Hand) (otherHand: Hand) =
-    let cardValue card =
-      match card with
-      | 'A' -> 14
-      | 'K' -> 13
-      | 'Q' -> 12
-      | 'J' -> 11
-      | 'T' -> 10
-      | c -> Int32.Parse(string c)
+  let cardValueWithJacks card =
+    match card with
+    | 'A' -> 14
+    | 'K' -> 13
+    | 'Q' -> 12
+    | 'J' -> 11
+    | 'T' -> 10
+    | c -> Int32.Parse(string c)
 
+  let kindWithJokers (hand: Hand) =
+    let counts = countCards hand
+    let jokerCount = counts |> Map.tryFind 'J' |> Option.defaultValue 0
+
+    if jokerCount = 5 then
+      Type.FiveOfAKind
+    else
+      let sortedCounts =
+        counts
+        |> Map.filter (fun k _ -> k <> 'J')
+        |> Map.toArray
+        |> Array.sortByDescending snd
+
+      match sortedCounts[0] with
+      | _, count when (count + jokerCount) = 5 -> Type.FiveOfAKind
+      | _, count when (count + jokerCount) = 4 -> Type.FourOfAKind
+      | _, count when (count + jokerCount) = 3 && (snd sortedCounts[1]) = 2 -> Type.FullHouse
+      | _, count when (count + jokerCount) = 3 -> Type.ThreeOfAKind
+      | _, count when (count + jokerCount) = 2 && (snd sortedCounts[1]) = 2 -> Type.TwoPair
+      | _, count when (count + jokerCount) = 2 -> Type.OnePair
+      | _ -> Type.HighCard
+
+  let cardValueWithJokers card =
+    match card with
+    | 'A' -> 14
+    | 'K' -> 13
+    | 'Q' -> 12
+    | 'T' -> 10
+    | 'J' -> 1
+    | c -> Int32.Parse(string c)
+
+  let compareHighCards (cardEval: char -> int) (hand: Hand) (otherHand: Hand) =
     let compareCards (card, otherCard) =
-      (cardValue card).CompareTo(cardValue otherCard)
+      (cardEval card).CompareTo(cardEval otherCard)
 
     Seq.zip (hand.ToCharArray()) (otherHand.ToCharArray())
-    |> Seq.tryFind (fun (card, otherCard) -> cardValue card <> cardValue otherCard)
+    |> Seq.tryFind (fun cards -> compareCards cards <> 0)
     |> Option.map compareCards
     |> Option.defaultValue 0
 
-  let strength (hand: Hand, _) (otherHand: Hand, _) =
-    let compareType = (hand |> kind |> int).CompareTo(otherHand |> kind |> int)
+  let strength kindEval cardEval hand otherHand =
+    let compareType = (hand |> kindEval |> int).CompareTo(otherHand |> kindEval |> int)
 
     if compareType <> 0 then
       compareType
     else
-      compareHighCards hand otherHand
+      compareHighCards cardEval hand otherHand
 
   let score rank (_, bid) = bid * (rank + 1)
 
+  let strengthWithJacks: (Hand -> Hand -> int) =
+    strength kindWithJacks cardValueWithJacks
+
+  let strengthWithJokers: (Hand -> Hand -> int) =
+    strength kindWithJokers cardValueWithJokers
+
 module CamelHands =
-  let totalWinnings (hands: CamelHands) =
-    hands |> Array.sortWith Hand.strength |> Array.mapi Hand.score |> Array.sum
+  let totalWinnings handEval (hands: CamelHands) =
+    hands
+    |> Array.sortWith (fun (hand, _) (otherHand, _) -> handEval hand otherHand)
+    |> Array.mapi Hand.score
+    |> Array.sum
 
   let parse filename =
     filename
--- AdventOfCode2023/Day7/Tests.fs
+++ AdventOfCode2023/Day7/Tests.fs
@@ -6,13 +6,21 @@ open Xunit
 [<InlineData("Day7/testInput.txt", 6440)>]
 [<InlineData("Day7/input.txt", 256448566)>]
 let ``The total winnings of the Camel Cards hands`` (filename: string, expected: int) =
-  let result = filename |> CamelHands.parse |> CamelHands.totalWinnings
+  let result =
+    filename
+    |> CamelHands.parse
+    |> (CamelHands.totalWinnings Hand.strengthWithJacks)
+
   Assert.Equal(expected, result)
 
 
 [<Theory>]
-[<InlineData("Day7/testInput.txt", -1)>]
-[<InlineData("Day7/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day7/testInput.txt", 5905)>]
+[<InlineData("Day7/input.txt", 254412181)>]
+let ``The total winnings of the Camel Cards hands with Jokers`` (filename: string, expected: int) =
+  let result =
+    filename
+    |> CamelHands.parse
+    |> (CamelHands.totalWinnings Hand.strengthWithJokers)
+
   Assert.Equal(expected, result)
