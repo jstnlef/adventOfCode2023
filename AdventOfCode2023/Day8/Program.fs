1a141d5d28f77a99d01e0dd4c2c2d8f455c65f6a
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-08 01:03:53 -0500

    Adding the beginning of day8 solution

--- AdventOfCode2023/Day8/Program.fs
+++ AdventOfCode2023/Day8/Program.fs
@@ -1,1 +1,32 @@
 namespace Day8
+
+open System.IO
+open System.Text.RegularExpressions
+
+type Network = Map<string, string * string>
+
+type WastelandMap =
+  { instructions: string
+    network: Network }
+
+module WastelandMap =
+  let startNode = "AAA"
+  let endNode = "ZZZ"
+
+  let findWayOut (map: WastelandMap) = 0
+
+  let parse filename =
+    let lineRegex = Regex("(?<node>\w+) = \((?<left>\w+), (?<right>\w+)\)")
+    let lines = filename |> File.ReadAllLines
+
+    let parseLine map line =
+      let m = lineRegex.Match line
+      let node = m.Groups["node"].Value
+      let left = m.Groups["left"].Value
+      let right = m.Groups["right"].Value
+      Map.add node (left, right) map
+
+    let network = lines[2..] |> Seq.fold parseLine Map.empty
+
+    { instructions = lines[0]
+      network = network }
--- AdventOfCode2023/Day8/Tests.fs
+++ AdventOfCode2023/Day8/Tests.fs
@@ -3,10 +3,11 @@ module Day8.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day8/testInput.txt", -1)>]
+[<InlineData("Day8/testInput.txt", 2)>]
+[<InlineData("Day8/testInput2.txt", 6)>]
 [<InlineData("Day8/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``Number of steps required to reach ZZZ`` (filename: string, expected: int) =
+  let result = filename |> WastelandMap.parse |> WastelandMap.findWayOut
   Assert.Equal(expected, result)
 
 
