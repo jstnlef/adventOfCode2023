c6795a273cb3759447c6ada5176272b95318df29
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-09 16:05:12 -0500

    Day9 part 1

--- AdventOfCode2023/Day9/Program.fs
+++ AdventOfCode2023/Day9/Program.fs
@@ -1,6 +1,33 @@
 namespace Day9
 
+open System
 open System.IO
 
+type History = int64 array
+
+type OASISReport = History array
+
 module OASISReport =
-  let parse filename = filename |> File.ReadLines
+  let delta history : int64 array =
+    history |> Array.windowed 2 |> Array.map (fun window -> window[1] - window[0])
+
+  let nextPredictedValue (history: History) : int64 =
+    seq {
+      let mutable d = history
+      yield d
+
+      while Array.sum d <> 0 do
+        d <- delta d
+        yield d
+
+    }
+    |> Seq.map Array.last
+    |> Seq.sum
+
+  let nextPredictedValues (report: OASISReport) = report |> Array.map nextPredictedValue
+
+  let parse filename =
+    filename
+    |> File.ReadLines
+    |> Seq.map (fun s -> s.Split(" ") |> Array.map Int64.Parse)
+    |> Seq.toArray
--- AdventOfCode2023/Day9/Tests.fs
+++ AdventOfCode2023/Day9/Tests.fs
@@ -4,15 +4,17 @@ open Xunit
 
 [<Theory>]
 [<InlineData("Day9/testInput.txt", 114)>]
-[<InlineData("Day9/input.txt", -1)>]
-let ``The sum of the extrapolated values`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day9/input.txt", 1930746032)>]
+let ``The sum of the extrapolated values`` (filename: string, expected: int64) =
+  let result =
+    filename |> OASISReport.parse |> OASISReport.nextPredictedValues |> Array.sum
+
   Assert.Equal(expected, result)
 
 
 [<Theory>]
 [<InlineData("Day9/testInput.txt", -1)>]
 [<InlineData("Day9/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
+let ``test 2`` (filename: string, expected: int64) =
   let result = 0
   Assert.Equal(expected, result)
