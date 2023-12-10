dd9a3e40ad625d1b80c506cd27aca20451f527ab
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-10 02:32:23 -0500

    Stubbing day 10 with parsing

--- AdventOfCode2023/Day10/Program.fs
+++ AdventOfCode2023/Day10/Program.fs
@@ -1,1 +1,35 @@
 namespace Day10
+
+open System.IO
+
+type Location =
+  | Pipe_NS
+  | Pipe_WE
+  | Pipe_NW
+  | Pipe_NE
+  | Pipe_SW
+  | Pipe_SE
+  | Ground
+  | Start
+
+module Location =
+  let convert c =
+    match c with
+    | '|' -> Pipe_NE
+    | '-' -> Pipe_WE
+    | 'L' -> Pipe_NE
+    | 'J' -> Pipe_NW
+    | '7' -> Pipe_SW
+    | 'F' -> Pipe_SE
+    | 'S' -> Start
+    | _ -> Ground
+
+type Pipes = Location array array
+
+module Pipes =
+  let distanceToFarthestPoint pipes = 0
+
+  let parse filename : Pipes =
+    filename
+    |> File.ReadAllLines
+    |> Array.map (fun line -> line |> Seq.map Location.convert |> Seq.toArray)
--- AdventOfCode2023/Day10/Tests.fs
+++ AdventOfCode2023/Day10/Tests.fs
@@ -3,10 +3,11 @@ module Day10.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day10/testInput.txt", -1)>]
+[<InlineData("Day10/testInput.txt", 4)>]
+[<InlineData("Day10/testInput2.txt", 8)>]
 [<InlineData("Day10/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``Number of steps to get to the farthest point in the pipe loop`` (filename: string, expected: int) =
+  let result = filename |> Pipes.parse |> Pipes.distanceToFarthestPoint
   Assert.Equal(expected, result)
 
 
