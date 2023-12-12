49e2f8b1cfeed28b82960ad111fca1edae716613
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-12 01:48:50 -0500

    doing some of day 11

--- AdventOfCode2023/Day11/Program.fs
+++ AdventOfCode2023/Day11/Program.fs
@@ -1,1 +1,37 @@
 namespace Day11
+
+open System.IO
+
+type Galaxy = { id: int; pos: int * int }
+
+type Image = List<Galaxy>
+
+module Image =
+  let rec comb n l =
+    match n, l with
+    | 0, _ -> [ [] ]
+    | _, [] -> []
+    | k, x :: xs -> List.map ((@) [ x ]) (comb (k - 1) xs) @ comb k xs
+
+  let distance (ax, ay) (bx, by) = abs (bx - ax) + abs (by - ay)
+
+  let allShortestDistances (image: Image) =
+    image
+    |> comb 2
+    |> Seq.map (fun galaxies -> distance (galaxies[0].pos) (galaxies[1].pos))
+
+  let parse filename : Image =
+    let mutable id = 0
+    let mutable galaxies = List.empty
+    let lines = filename |> File.ReadLines
+
+    for y, line in Seq.indexed lines do
+      for x, c in Seq.indexed line do
+        match c with
+        | '#' ->
+          id <- id + 1
+          let galaxy = { id = id; pos = (x, y) }
+          galaxies <- galaxies @ [ galaxy ]
+        | _ -> ()
+
+    galaxies
--- AdventOfCode2023/Day11/Tests.fs
+++ AdventOfCode2023/Day11/Tests.fs
@@ -3,10 +3,10 @@ module Day11.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day11/testInput.txt", -1)>]
+[<InlineData("Day11/testInput.txt", 374)>]
 [<InlineData("Day11/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``The sum of these lengths between each pair of galaxies`` (filename: string, expected: int) =
+  let result = filename |> Image.parse |> Image.allShortestDistances |> Seq.sum
   Assert.Equal(expected, result)
 
 
