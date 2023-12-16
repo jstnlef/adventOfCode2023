7d52828d28e8aac5f3c62047177673fc822dab08
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-16 01:45:05 -0500

    Parsing Day 12

--- AdventOfCode2023/Day12/Program.fs
+++ AdventOfCode2023/Day12/Program.fs
@@ -1,1 +1,43 @@
 namespace Day12
+
+open System
+open System.IO
+open System.Text.RegularExpressions
+
+type Condition =
+  | Unknown
+  | Damaged
+  | Operational
+
+module Condition =
+  let parse c =
+    match c with
+    | '?' -> Unknown
+    | '.' -> Operational
+    | '#' -> Damaged
+    | _ -> failwith "Unknown character"
+
+type Springs =
+  { springs: Condition array
+    groups: int array }
+
+type ConditionRecord = Springs array
+
+module ConditionRecord =
+  let numberOfArrangementsPerRow record = Seq.empty
+
+  let parse filename : ConditionRecord =
+    let regex = Regex("^(?<springs>[\?\.\#]+) ((?<groups>\d+),?)+$")
+
+    let parseLine line =
+      let m = regex.Match line
+      let springs = m.Groups["springs"].Value |> Seq.map Condition.parse |> Seq.toArray
+
+      let groups =
+        m.Groups["groups"].Captures
+        |> Seq.map (fun c -> Int32.Parse c.Value)
+        |> Seq.toArray
+
+      { springs = springs; groups = groups }
+
+    filename |> File.ReadLines |> Seq.map parseLine |> Seq.toArray
--- AdventOfCode2023/Day12/Tests.fs
+++ AdventOfCode2023/Day12/Tests.fs
@@ -3,10 +3,15 @@ module Day12.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day12/testInput.txt", -1)>]
+[<InlineData("Day12/testInput.txt", 21)>]
 [<InlineData("Day12/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``The sum of the different arrangements of springs`` (filename: string, expected: int) =
+  let result =
+    filename
+    |> ConditionRecord.parse
+    |> ConditionRecord.numberOfArrangementsPerRow
+    |> Seq.sum
+
   Assert.Equal(expected, result)
 
 
