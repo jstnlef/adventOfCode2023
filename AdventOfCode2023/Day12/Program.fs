4a125265461e0ab6163376771fe72e93c4162143
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-16 18:16:48 -0500

    Some day 12 refactoring

--- AdventOfCode2023/Day12/Program.fs
+++ AdventOfCode2023/Day12/Program.fs
@@ -5,36 +5,29 @@ open System.IO
 open System.Text.RegularExpressions
 open AdventOfCode2023.Common
 
-type Springs = { springs: string; groups: int list }
+type Springs = string * int list
 
 type ConditionRecord = Springs array
 
 module rec ConditionRecord =
-  let rec countArrangements (springs: string, groups) : int64 =
+  let countArrangements (springs: string, groups) : int64 =
     match groups with
-    | [] -> if (Seq.contains '#' springs) then 0 else 1
+    | [] -> if springs.Contains('#') then 0 else 1
     | groupHead :: restOfGroup ->
-      seq { 0 .. (springs.Length - (List.sum restOfGroup) - (List.length restOfGroup) - groupHead) }
-      |> Seq.takeWhile (fun i -> not (Seq.contains '#' springs[.. i - 1]))
-      |> Seq.fold
-        (fun result i ->
-          let next = i + groupHead
+      seq { 0 .. springs.Length - (List.sum restOfGroup) - restOfGroup.Length - groupHead }
+      |> Seq.takeWhile (fun i -> not (springs[.. i - 1].Contains('#')))
+      |> Seq.filter (fun i ->
+        let next = i + groupHead
 
-          let lessThanLength = next - 1 <= springs.Length
-          let noOperatingSprings = not (Seq.contains '.' springs[i .. next - 1])
-          let nextNotDamaged = springs[next..next] <> "#"
-
-          if lessThanLength && noOperatingSprings && nextNotDamaged then
-            result + numberOfArrangements (springs[next + 1 ..], restOfGroup)
-          else
-            result)
-        0
+        let lessThanLength = next - 1 <= springs.Length
+        let noOperatingSprings = not (springs[i .. next - 1].Contains('.'))
+        let nextNotDamaged = springs[next..next] <> "#"
+        lessThanLength && noOperatingSprings && nextNotDamaged)
+      |> Seq.fold (fun result i -> result + numberOfArrangements (springs[i + groupHead + 1 ..], restOfGroup)) 0
 
   let numberOfArrangements = Functools.memoize countArrangements
 
-  let numberOfArrangementsPerRow record =
-    record
-    |> Seq.map (fun { springs = springs; groups = groups } -> numberOfArrangements (springs, groups))
+  let numberOfArrangementsPerRow record = record |> Seq.map numberOfArrangements
 
   let parse expand filename : ConditionRecord =
     let regex = Regex("^(?<springs>[\?\.\#]+) ((?<groups>\d+),?)+$")
@@ -56,6 +49,6 @@ module rec ConditionRecord =
       if expand then
         groups <- groups |> Seq.replicate 5 |> List.concat
 
-      { springs = springs; groups = groups }
+      springs, groups
 
     filename |> File.ReadLines |> Seq.map parseLine |> Seq.toArray
