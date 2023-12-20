397d3c1577cece53529beb6430274f7afeabcc53
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-20 17:05:54 -0500

    Day 14 part 1 complete

--- AdventOfCode2023/Day14/Program.fs
+++ AdventOfCode2023/Day14/Program.fs
@@ -1,1 +1,33 @@
 namespace Day14
+
+open System
+open System.IO
+
+type Platform = char array array
+
+module Platform =
+  let mirror = 'O'
+  let rock = '#'
+  let ground = '.'
+
+  let roll (platform: Platform) : Platform =
+    for c in 0 .. platform[0].Length - 1 do
+      for _ in 1 .. platform.Length - 1 do
+        for r in 1 .. platform.Length - 1 do
+          if platform[r][c] = mirror && platform[r - 1][c] = ground then
+            platform[r][c] <- ground
+            platform[r - 1][c] <- mirror
+
+    platform
+
+  let determineLoad (platform: Platform) : int =
+    platform
+    |> Array.indexed
+    |> Array.sumBy (fun (i, row) ->
+      (platform.Length - i)
+      * (row |> Array.sumBy (fun c -> Convert.ToInt32((c = mirror)))))
+
+  let loadWhenMovedNorth = roll >> determineLoad
+
+  let parse filename : Platform =
+    filename |> File.ReadAllLines |> Array.map _.ToCharArray()
--- AdventOfCode2023/Day14/Tests.fs
+++ AdventOfCode2023/Day14/Tests.fs
@@ -3,10 +3,10 @@ module Day14.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day14/testInput.txt", -1)>]
-[<InlineData("Day14/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day14/testInput.txt", 136)>]
+[<InlineData("Day14/input.txt", 106186)>]
+let ``The total load on the north support beams`` (filename: string, expected: int) =
+  let result = filename |> Platform.parse |> Platform.loadWhenMovedNorth
   Assert.Equal(expected, result)
 
 
