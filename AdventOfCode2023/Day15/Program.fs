acb292eb1d8d3c123530766da664b53011a6d227
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-21 17:53:01 -0500

    Day15 part 1

--- AdventOfCode2023/Day15/Program.fs
+++ AdventOfCode2023/Day15/Program.fs
@@ -1,1 +1,11 @@
 namespace Day15
+
+open System.IO
+
+module Initialization =
+  let hash = Seq.fold (fun current c -> (current + int c) * 17 % 256) 0
+
+  let hashAllSteps: string array -> int = Array.sumBy hash
+
+  let parse filename =
+    filename |> File.ReadAllText |> _.Trim().Split(",")
--- AdventOfCode2023/Day15/Tests.fs
+++ AdventOfCode2023/Day15/Tests.fs
@@ -3,10 +3,10 @@ module Day15.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day15/testInput.txt", -1)>]
-[<InlineData("Day15/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day15/testInput.txt", 1320)>]
+[<InlineData("Day15/input.txt", 506869)>]
+let ``The sum of the hashes of each of the initialization steps`` (filename: string, expected: int) =
+  let result = filename |> Initialization.parse |> Initialization.hashAllSteps
   Assert.Equal(expected, result)
 
 
