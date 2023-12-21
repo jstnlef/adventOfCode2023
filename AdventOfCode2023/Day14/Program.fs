b5c91295bacc782938a6724cb0d3454b5d555907
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-20 21:49:37 -0500

    Ugh. Part 2 day 14 done after all sorts of pain

--- AdventOfCode2023/Day14/Program.fs
+++ AdventOfCode2023/Day14/Program.fs
@@ -11,16 +11,27 @@ module Platform =
   let ground = '.'
 
   let roll (platform: Platform) : Platform =
-    for c in 0 .. platform[0].Length - 1 do
-      for _ in 1 .. platform.Length - 1 do
-        for r in 1 .. platform.Length - 1 do
-          if platform[r][c] = mirror && platform[r - 1][c] = ground then
-            platform[r][c] <- ground
-            platform[r - 1][c] <- mirror
+    let n = Array.init platform.Length (fun i -> Array.copy platform[i])
 
-    platform
+    for c in 0 .. n[0].Length - 1 do
+      for _ in 1 .. n.Length - 1 do
+        for r in 1 .. n.Length - 1 do
+          if n[r][c] = mirror && n[r - 1][c] = ground then
+            n[r][c] <- ground
+            n[r - 1][c] <- mirror
 
-  let determineLoad (platform: Platform) : int =
+    n
+
+  let rotate (platform: Platform) : Platform =
+    let n = Array.init platform.Length (fun _ -> Array.zeroCreate platform[0].Length)
+
+    for r in 0 .. n.Length - 1 do
+      for c in 0 .. n.Length - 1 do
+        n[c][platform.Length - 1 - r] <- platform[r][c]
+
+    n
+
+  let determineLoad (platform: Platform) =
     platform
     |> Array.indexed
     |> Array.sumBy (fun (i, row) ->
@@ -28,6 +39,35 @@ module Platform =
       * (row |> Array.sumBy (fun c -> Convert.ToInt32((c = mirror)))))
 
   let loadWhenMovedNorth = roll >> determineLoad
+
+  let runCycle (platform: Platform) =
+    seq { 0..3 } |> Seq.fold (fun p _ -> p |> roll |> rotate) platform
+
+  let loadAfterNCycles cycles platform =
+    let mutable m = Map.empty
+    let mutable p = platform
+    let mutable currentCycle = 0
+    let mutable foundCycle = false
+
+    while not foundCycle && currentCycle < cycles do
+      p <-
+        match m.TryFind(p) with
+        | None ->
+          let next = runCycle p
+          m <- m.Add(p, (next, currentCycle + 1))
+          next
+        | Some(_, repeatStartCycle) ->
+          foundCycle <- true
+          let lengthOfRepeat = m.Count - repeatStartCycle + 1
+
+          let solutionCycle =
+            repeatStartCycle + ((cycles - currentCycle) % lengthOfRepeat) - 1
+
+          m.Values |> Seq.find (fun (_, index) -> index = solutionCycle) |> fst
+
+      currentCycle <- currentCycle + 1
+
+    determineLoad p
 
   let parse filename : Platform =
     filename |> File.ReadAllLines |> Array.map _.ToCharArray()
--- AdventOfCode2023/Day14/Tests.fs
+++ AdventOfCode2023/Day14/Tests.fs
@@ -11,8 +11,8 @@ let ``The total load on the north support beams`` (filename: string, expected: i
 
 
 [<Theory>]
-[<InlineData("Day14/testInput.txt", -1)>]
-[<InlineData("Day14/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day14/testInput.txt", 64)>]
+[<InlineData("Day14/input.txt", 106390)>]
+let ``The total load on the north support beams after many spin cycles`` (filename: string, expected: int) =
+  let result = filename |> Platform.parse |> Platform.loadAfterNCycles 1_000_000_000
   Assert.Equal(expected, result)
