98efcb0321fc9c8655085544c08dd6092df279c7
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-22 02:03:02 -0500

    Starting day 16

--- AdventOfCode2023/Day16/Program.fs
+++ AdventOfCode2023/Day16/Program.fs
@@ -1,1 +1,11 @@
 namespace Day16
+
+open System.IO
+
+type Contraption = char array array
+
+module Contraption =
+  let countEnergizedTiles contraption = 0
+
+  let parse filename : Contraption =
+    filename |> File.ReadAllLines |> Array.map _.ToCharArray()
--- AdventOfCode2023/Day16/Tests.fs
+++ AdventOfCode2023/Day16/Tests.fs
@@ -3,10 +3,10 @@ module Day16.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day16/testInput.txt", -1)>]
+[<InlineData("Day16/testInput.txt", 46)>]
 [<InlineData("Day16/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
-  let result = 0
+let ``Count the number of energized tiles`` (filename: string, expected: int) =
+  let result = filename |> Contraption.parse |> Contraption.countEnergizedTiles
   Assert.Equal(expected, result)
 
 
