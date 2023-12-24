cb69f314fefa3930936496983179f94e8600d878
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-24 14:28:32 -0500

    day 17 part 2 complete

--- AdventOfCode2023/Day17/Tests.fs
+++ AdventOfCode2023/Day17/Tests.fs
@@ -11,8 +11,13 @@ let ``The least amount of heat loss while traversing the city`` (filename: strin
 
 
 [<Theory>]
-[<InlineData("Day17/testInput.txt", -1)>]
-[<InlineData("Day17/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day17/testInput.txt", 94)>]
+[<InlineData("Day17/testInput2.txt", 71)>]
+[<InlineData("Day17/input.txt", 1367)>]
+let ``The least amount of heat loss while traversing the city with an ultra crucible``
+  (
+    filename: string,
+    expected: int
+  ) =
+  let result = filename |> HeatLossMap.parse |> HeatLossMap.findMinHeatLoss 4 10
   Assert.Equal(expected, result)
