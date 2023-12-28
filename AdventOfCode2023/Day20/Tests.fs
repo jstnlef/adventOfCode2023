d34a2dc5e05e6a9d678536d2226eedacc96c0fe6
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-28 14:15:42 -0500

    Adding day20 inputs

--- AdventOfCode2023/Day20/Tests.fs
+++ AdventOfCode2023/Day20/Tests.fs
@@ -3,9 +3,10 @@ module Day20.Tests
 open Xunit
 
 [<Theory>]
-[<InlineData("Day20/testInput.txt", -1)>]
+[<InlineData("Day20/testInput.txt", 32000000)>]
+[<InlineData("Day20/testInput2.txt", 11687500)>]
 [<InlineData("Day20/input.txt", -1)>]
-let ``test`` (filename: string, expected: int) =
+let ``Multiply the total number of low pulses and high pulses`` (filename: string, expected: int64) =
   let result = 0
   Assert.Equal(expected, result)
 
