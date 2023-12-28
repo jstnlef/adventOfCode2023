adc71e83fbf2e8839c76b37f7b375953b32d02c1
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-28 13:45:28 -0500

    Day 19 part 2 complete

--- AdventOfCode2023/Day19/Program.fs
+++ AdventOfCode2023/Day19/Program.fs
@@ -31,13 +31,18 @@ module Op =
 type Rule =
   { category: string
     op: Op
-    value: int
+    value: int64
     transition: Transition }
 
-type Part = { x: int; m: int; a: int; s: int }
+type Part =
+  { x: int64
+    m: int64
+    a: int64
+    s: int64 }
 
 module Part =
   let ratingSum part = part.x + part.m + part.a + part.s
+
   let regex = Regex("^{x=(?<x>\d+),m=(?<m>\d+),a=(?<a>\d+),s=(?<s>\d+)}$")
 
   let parse line =
@@ -48,8 +53,38 @@ module Part =
       a = Int32.Parse(m.Groups["a"].Value)
       s = Int32.Parse(m.Groups["s"].Value) }
 
+type PartRange =
+  { x: int64 * int64
+    m: int64 * int64
+    a: int64 * int64
+    s: int64 * int64 }
+
 module Rule =
-  let evaluate part rule =
+  let evaluateRuleForRange (range: PartRange) rule : (Transition * PartRange) * PartRange =
+    let split (rMin, rMax) =
+      match rule.op with
+      | GreaterThan -> ((max (rule.value + 1L) rMin), rMax), (rMin, (min rule.value rMax))
+      | LessThan -> (rMin, (min (rule.value - 1L) rMax)), ((max rule.value rMin), rMax)
+
+    let matched, unmatched =
+      match rule.category with
+      | "x" ->
+        let a, r = split range.x
+        { range with x = a }, { range with x = r }
+      | "m" ->
+        let a, r = split range.m
+        { range with m = a }, { range with m = r }
+      | "a" ->
+        let a, r = split range.a
+        { range with a = a }, { range with a = r }
+      | "s" ->
+        let a, r = split range.s
+        { range with s = a }, { range with s = r }
+      | _ -> failwith "Unknown category"
+
+    (rule.transition, matched), unmatched
+
+  let evaluate (part: Part) rule =
     let rating =
       match rule.category with
       | "x" -> part.x
@@ -81,6 +116,36 @@ module Workflow =
 
     evaluateWithWorkflow workflows["in"]
 
+  let ratingMin = 1L
+  let ratingMax = 4000L
+
+  let findAcceptedRatingsRanges (workflows: Map<string, Workflow>) =
+    let rec findRangesWithWorkflow ranges =
+      match ranges with
+      | [] -> []
+      | (Rejected, _) :: rest -> findRangesWithWorkflow rest
+      | (Accepted, range) :: rest -> (Accepted, range) :: findRangesWithWorkflow rest
+      | (Rule(name), range) :: rest ->
+        let workflow = workflows[name]
+        let mutable nextToEval = range
+        let mutable nextRanges = rest
+
+        for rule in workflow.rules do
+          let a, b = Rule.evaluateRuleForRange nextToEval rule
+          nextRanges <- a :: nextRanges
+          nextToEval <- b
+
+        nextRanges <- (workflow.defaultTransition, nextToEval) :: nextRanges
+        findRangesWithWorkflow nextRanges
+
+    let initRange =
+      { x = ratingMin, ratingMax
+        m = ratingMin, ratingMax
+        a = ratingMin, ratingMax
+        s = ratingMin, ratingMax }
+
+    findRangesWithWorkflow [ (Rule("in"), initRange) ] |> List.map snd
+
   let regex =
     Regex("^(?<name>\w+){(?:(?<cat>\w)(?<op>[<|>])(?<num>\d+):(?<transition>\w+),*)+(?<defaultTransition>\w+)}$")
 
@@ -92,7 +157,7 @@ module Workflow =
       |> Seq.map (fun i ->
         { category = m.Groups["cat"].Captures[i].Value
           op = m.Groups["op"].Captures[i].Value |> Op.parse
-          value = m.Groups["num"].Captures[i].Value |> Int32.Parse
+          value = m.Groups["num"].Captures[i].Value |> Int64.Parse
           transition = m.Groups["transition"].Captures[i].Value |> Transition.parse })
       |> Seq.toArray
 
@@ -109,6 +174,20 @@ module System =
     system.parts
     |> Array.filter (fun part -> Workflow.evaluate part system.workflows = Accepted)
     |> Array.sumBy Part.ratingSum
+
+  let combinationsOfAcceptedRatings system : int64 =
+    system.workflows
+    |> Workflow.findAcceptedRatingsRanges
+    |> Seq.sumBy (fun ranges ->
+      let xMin, xMax = ranges.x
+      let mMin, mMax = ranges.m
+      let aMin, aMax = ranges.a
+      let sMin, sMax = ranges.s
+
+      (xMax - xMin + 1L)
+      * (mMax - mMin + 1L)
+      * (aMax - aMin + 1L)
+      * (sMax - sMin + 1L))
 
   let parse filename =
     let lines = filename |> File.ReadAllLines
--- AdventOfCode2023/Day19/Tests.fs
+++ AdventOfCode2023/Day19/Tests.fs
@@ -5,14 +5,14 @@ open Xunit
 [<Theory>]
 [<InlineData("Day19/testInput.txt", 19114)>]
 [<InlineData("Day19/input.txt", 399284)>]
-let ``Add together all of the rating numbers for all of the accepted parts`` (filename: string, expected: int) =
+let ``Add together all of the rating numbers for all of the accepted parts`` (filename: string, expected: int64) =
   let result = filename |> System.parse |> System.sumOfAcceptedParts
   Assert.Equal(expected, result)
 
 
 [<Theory>]
-[<InlineData("Day19/testInput.txt", -1)>]
-[<InlineData("Day19/input.txt", -1)>]
-let ``test 2`` (filename: string, expected: int) =
-  let result = 0
+[<InlineData("Day19/testInput.txt", 167409079868000L)>]
+[<InlineData("Day19/input.txt", 121964982771486L)>]
+let ``Number of distinct combinations of parts ratings which will be accepted`` (filename: string, expected: int64) =
+  let result = filename |> System.parse |> System.combinationsOfAcceptedRatings
   Assert.Equal(expected, result)
