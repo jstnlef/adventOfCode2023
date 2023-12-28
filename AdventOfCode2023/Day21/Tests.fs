1d076d41838dfa26e80a2587d36ecc6fb1dd4643
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-28 13:49:01 -0500

    Stub out day 21

---
+++ AdventOfCode2023/Day21/Tests.fs
@@ -0,0 +1,18 @@
+module Day21.Tests
+
+open Xunit
+
+[<Theory>]
+[<InlineData("Day21/testInput.txt", -1)>]
+[<InlineData("Day21/input.txt", -1)>]
+let ``test`` (filename: string, expected: int) =
+  let result = 0
+  Assert.Equal(expected, result)
+
+
+[<Theory>]
+[<InlineData("Day21/testInput.txt", -1)>]
+[<InlineData("Day21/input.txt", -1)>]
+let ``test 2`` (filename: string, expected: int) =
+  let result = 0
+  Assert.Equal(expected, result)
