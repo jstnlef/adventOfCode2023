89ef0819d68244d61f1364402003e741ca1df0c7
Justin LeFebvre <jstnlefebvre@gmail.com>
2023-12-29 16:22:59 -0500

    Day 20 part 1 complete

--- AdventOfCode2023/Day20/Program.fs
+++ AdventOfCode2023/Day20/Program.fs
@@ -1,26 +1,156 @@
 namespace Day20
 
+open System.Collections.Generic
 open System.IO
 open System.Text.RegularExpressions
 
-type Broadcaster = { outputs: string array }
-type FlipFlop = { outputs: string array }
-type Conjunction = { outputs: string array }
+type Broadcaster = { name: string; outputs: string array }
+
+type FlipFlop =
+  { name: string
+    isOn: bool
+    outputs: string array }
+
+  member this.Toggle() = { this with isOn = not this.isOn }
+
+type Voltage =
+  | High
+  | Low
+
+type Pulse =
+  { src: string
+    dest: string
+    voltage: Voltage }
+
+type Conjunction =
+  { name: string
+    memory: Map<string, Voltage>
+    outputs: string array }
+
+  member this.UpdateMemory(pulse: Pulse) =
+    { this with
+        memory =
+          Map.change
+            pulse.src
+            (fun maybeCharge ->
+              match maybeCharge with
+              | Some _ -> Some(pulse.voltage)
+              | None -> failwith "unknown input!")
+            this.memory }
 
 type Module =
   | Broadcaster of Broadcaster
   | FlipFlop of FlipFlop
   | Conjunction of Conjunction
 
-type ModuleConfig = Map<string, Module>
+type Modules = Map<string, Module>
 
-module ModuleConfig =
-  let countPulses config = 0
+type State =
+  { pulses: Queue<Pulse>
+    modules: Modules
+    lowPulseCount: int
+    highPulseCount: int }
+
+  member this.PushButton() =
+    this.SendPulse(
+      { src = "button"
+        dest = "broadcaster"
+        voltage = Low }
+    )
+
+  member this.SendPulse(pulse: Pulse) =
+    this.pulses.Enqueue(pulse)
+
+    match pulse.voltage with
+    | Low ->
+      { this with
+          lowPulseCount = this.lowPulseCount + 1 }
+    | High ->
+      { this with
+          highPulseCount = this.highPulseCount + 1 }
+
+  member this.UpdateModule(name: string, m: Module) =
+    { this with
+        modules = this.modules.Add(name, m) }
+
+module Modules =
+  let init modules : State =
+    { pulses = Queue()
+      modules = modules
+      lowPulseCount = 0
+      highPulseCount = 0 }
+
+  let doBroadcast (broadcast: Broadcaster) state =
+    broadcast.outputs
+    |> Array.fold
+      (fun (s: State) output ->
+        s.SendPulse(
+          { src = broadcast.name
+            dest = output
+            voltage = Low }
+        ))
+      state
+
+  let doFlipFlop (flipFlop: FlipFlop) pulse (state: State) =
+    if pulse.voltage = Low then
+      let updated = flipFlop.Toggle()
+
+      updated.outputs
+      |> Array.fold
+        (fun (s: State) output ->
+          s.SendPulse(
+            { src = updated.name
+              dest = output
+              voltage = if updated.isOn then High else Low }
+          ))
+        (state.UpdateModule(flipFlop.name, FlipFlop(updated)))
+    else
+      state
+
+  let doConjunction (conjunction: Conjunction) pulse (state: State) =
+    let updated = conjunction.UpdateMemory(pulse)
+
+    let pulseVoltage =
+      if updated.memory.Values |> Seq.forall (fun v -> v = High) then
+        Low
+      else
+        High
+
+    updated.outputs
+    |> Array.fold
+      (fun (s: State) output ->
+        s.SendPulse(
+          { src = updated.name
+            dest = output
+            voltage = pulseVoltage }
+        ))
+      (state.UpdateModule(updated.name, Conjunction(updated)))
+
+  let processPulse pulse state =
+    match Map.tryFind pulse.dest state.modules with
+    | None -> state
+    | Some(Broadcaster b) -> doBroadcast b state
+    | Some(FlipFlop f) -> doFlipFlop f pulse state
+    | Some(Conjunction c) -> doConjunction c pulse state
+
+  let pushButton (initial: State) _ =
+    let mutable state = initial.PushButton()
+    let mutable pulse = { src = ""; dest = ""; voltage = Low }
+
+    while state.pulses.TryDequeue(&pulse) do
+      state <- processPulse pulse state
+
+    state
+
+  let countPulses modules =
+    seq { 0..999 }
+    |> Seq.fold pushButton (init modules)
+    |> (fun s -> s.lowPulseCount * s.highPulseCount)
 
   let lineRegex =
     Regex("^(?<modType>broadcaster|[%|&])(?<name>\w+)* ->(?: (?<output>\w+),*)+$")
 
-  let parse filename : ModuleConfig =
+  let parse filename : Modules =
     let parseLine config line =
       let m = lineRegex.Match(line)
       let mutable name = m.Groups["name"].Value
@@ -30,11 +160,56 @@ module ModuleConfig =
         match m.Groups["modType"].Value with
         | "broadcaster" ->
           name <- "broadcaster"
-          Broadcaster { outputs = outputs }
-        | "%" -> FlipFlop { outputs = outputs }
-        | "&" -> Conjunction { outputs = outputs }
+          Broadcaster { name = name; outputs = outputs }
+        | "%" ->
+          FlipFlop
+            { name = name
+              isOn = false
+              outputs = outputs }
+        | "&" ->
+          Conjunction
+            { name = name
+              memory = Map.empty
+              outputs = outputs }
         | _ -> failwith "Unknown module type"
 
       config |> Map.add name moduleType
 
-    filename |> File.ReadLines |> Seq.fold parseLine Map.empty
+    let setConjunctionInputs (modules: Modules) =
+      let conjunctions =
+        modules.Values
+        |> Seq.map (fun m ->
+          match m with
+          | Module.Conjunction c -> c.name
+          | _ -> "")
+        |> Seq.filter (fun s -> s <> "")
+        |> Set
+
+      modules.Values
+      |> Seq.fold
+        (fun updated m ->
+          let name, outputs =
+            match m with
+            | Broadcaster b -> b.name, Set b.outputs
+            | FlipFlop f -> f.name, Set f.outputs
+            | Conjunction c -> c.name, Set c.outputs
+
+          Set.intersect conjunctions outputs
+          |> Set.fold
+            (fun (updated: Modules) n ->
+              let c =
+                match updated[n] with
+                | Conjunction c ->
+                  Conjunction
+                    { c with
+                        memory = Map.add name Low c.memory }
+                | _ -> failwith "Shouldn't be here!"
+
+              Map.add n c updated)
+            updated)
+        modules
+
+    filename
+    |> File.ReadLines
+    |> Seq.fold parseLine Map.empty
+    |> setConjunctionInputs
--- AdventOfCode2023/Day20/Tests.fs
+++ AdventOfCode2023/Day20/Tests.fs
@@ -5,9 +5,9 @@ open Xunit
 [<Theory>]
 [<InlineData("Day20/testInput.txt", 32000000)>]
 [<InlineData("Day20/testInput2.txt", 11687500)>]
-[<InlineData("Day20/input.txt", -1)>]
+[<InlineData("Day20/input.txt", 867118762)>]
 let ``Multiply the total number of low pulses and high pulses`` (filename: string, expected: int64) =
-  let result = filename |> ModuleConfig.parse |> ModuleConfig.countPulses
+  let result = filename |> Modules.parse |> Modules.countPulses
   Assert.Equal(expected, result)
 
 
